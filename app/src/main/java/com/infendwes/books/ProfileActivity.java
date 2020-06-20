b20a2bd84f6c1a2ef50459a3016ae13d5426624c
Ashok Reddy <kallamashokreddy23@gmail.com>
2020-06-20 21:30:29 +0530

    Profile Init 1/2

--- app/src/main/java/com/infendwes/books/ProfileActivity.java
+++ app/src/main/java/com/infendwes/books/ProfileActivity.java
@@ -7,15 +7,71 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView PName,PBadge;
+    CircleImageView PProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        PName = findViewById(R.id.P_name);
+        PBadge = findViewById(R.id.P_Badge);
+        PProfile = findViewById(R.id.P_Profile);
+
+        PBadge.setVisibility(View.INVISIBLE);
+
+        db.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            PName.setText(document.get("name").toString());
+                            String verify = document.get("verify").toString();
+
+                            if (document.get("profile").toString().trim().equals("default")){
+
+                            }
+                            else{
+                                Glide.with(ProfileActivity.this).load(document.get("profile").toString()).into(PProfile);
+                            }
+
+                            if (verify.equals("verified")){
+                                PBadge.setVisibility(View.VISIBLE);
+                            }
+
+
+                        }else {
+                            Toast.makeText(ProfileActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary, this.getTheme()));
